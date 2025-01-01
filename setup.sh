6e1ac13bb86fa719c07b290ea4907d06174cc77f
kimycai <17998728+kimycai@users.noreply.github.com>
2025-01-01 21:42:19 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -562,9 +562,10 @@ update_core(){
 menu() {
     clear
     echo "#############################################################"
-
+    echo -e "#                  ${RED}Hysteria 2 一键安装脚本${PLAIN}                  #"
+ 
     echo "#############################################################"
-    echo ""
+   
     echo -e " ${GREEN}1.${PLAIN} 安装 Hysteria 2"
     echo -e " ${GREEN}2.${PLAIN} ${RED}卸载 Hysteria 2${PLAIN}"
     echo " -------------"
